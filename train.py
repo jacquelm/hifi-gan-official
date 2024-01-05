51aeff2707472b6a9bfe0c9a22e0388b130ad86d
jacquelm <m.jacquelin@vogo.fr>
2024-01-05 10:44:15 +0100

    script to create mel datasets

--- train.py
+++ train.py
@@ -158,196 +158,197 @@ def train(rank, a, h):
     generator.train()
     mpd.train()
     msd.train()
-    for epoch in range(max(0, last_epoch), a.training_epochs):
-        if rank == 0:
-            start = time.time()
-            print("Epoch: {}".format(epoch + 1))
+    print(train_loader)
+    print(train_loader[0])
+    # for epoch in range(max(0, last_epoch), a.training_epochs):
+    #     if rank == 0:
+    #         start = time.time()
+    #         print("Epoch: {}".format(epoch + 1))
 
-        if h.num_gpus > 1:
-            train_sampler.set_epoch(epoch)
+    #     if h.num_gpus > 1:
+    #         train_sampler.set_epoch(epoch)
 
-        for batch in tqdm(train_loader):
-            print(len(batch))
-            if rank == 0:
-                start_b = time.time()
-            x, y, _, y_mel = batch
-            x = torch.autograd.Variable(x.to(device, non_blocking=True))
-            y = torch.autograd.Variable(y.to(device, non_blocking=True))
-            y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
-            y = y.unsqueeze(1)
+    #     for batch in tqdm(train_loader):
+    #         if rank == 0:
+    #             start_b = time.time()
+    #         x, y, _, y_mel = batch
+    #         x = torch.autograd.Variable(x.to(device, non_blocking=True))
+    #         y = torch.autograd.Variable(y.to(device, non_blocking=True))
+    #         y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
+    #         y = y.unsqueeze(1)
 
-            y_g_hat = generator(x)
-            y_g_hat_mel = mel_spectrogram(
-                y_g_hat.squeeze(1),
-                h.n_fft,
-                h.num_mels,
-                h.sampling_rate,
-                h.hop_size,
-                h.win_size,
-                h.fmin,
-                h.fmax_for_loss,
-            )
+    #         y_g_hat = generator(x)
+    #         y_g_hat_mel = mel_spectrogram(
+    #             y_g_hat.squeeze(1),
+    #             h.n_fft,
+    #             h.num_mels,
+    #             h.sampling_rate,
+    #             h.hop_size,
+    #             h.win_size,
+    #             h.fmin,
+    #             h.fmax_for_loss,
+    #         )
 
-            optim_d.zero_grad()
+    #         optim_d.zero_grad()
 
-            # MPD
-            y_df_hat_r, y_df_hat_g, _, _ = mpd(y, y_g_hat.detach())
-            loss_disc_f, losses_disc_f_r, losses_disc_f_g = discriminator_loss(
-                y_df_hat_r, y_df_hat_g
-            )
+    #         # MPD
+    #         y_df_hat_r, y_df_hat_g, _, _ = mpd(y, y_g_hat.detach())
+    #         loss_disc_f, losses_disc_f_r, losses_disc_f_g = discriminator_loss(
+    #             y_df_hat_r, y_df_hat_g
+    #         )
 
-            # MSD
-            y_ds_hat_r, y_ds_hat_g, _, _ = msd(y, y_g_hat.detach())
-            loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(
-                y_ds_hat_r, y_ds_hat_g
-            )
+    #         # MSD
+    #         y_ds_hat_r, y_ds_hat_g, _, _ = msd(y, y_g_hat.detach())
+    #         loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(
+    #             y_ds_hat_r, y_ds_hat_g
+    #         )
 
-            loss_disc_all = loss_disc_s + loss_disc_f
+    #         loss_disc_all = loss_disc_s + loss_disc_f
 
-            loss_disc_all.backward()
-            optim_d.step()
+    #         loss_disc_all.backward()
+    #         optim_d.step()
 
-            # Generator
-            optim_g.zero_grad()
+    #         # Generator
+    #         optim_g.zero_grad()
 
-            # L1 Mel-Spectrogram Loss
-            loss_mel = F.l1_loss(y_mel, y_g_hat_mel) * 45
+    #         # L1 Mel-Spectrogram Loss
+    #         loss_mel = F.l1_loss(y_mel, y_g_hat_mel) * 45
 
-            y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
-            y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
-            loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
-            loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
-            loss_gen_f, losses_gen_f = generator_loss(y_df_hat_g)
-            loss_gen_s, losses_gen_s = generator_loss(y_ds_hat_g)
-            loss_gen_all = loss_gen_s + loss_gen_f + loss_fm_s + loss_fm_f + loss_mel
+    #         y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
+    #         y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
+    #         loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
+    #         loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
+    #         loss_gen_f, losses_gen_f = generator_loss(y_df_hat_g)
+    #         loss_gen_s, losses_gen_s = generator_loss(y_ds_hat_g)
+    #         loss_gen_all = loss_gen_s + loss_gen_f + loss_fm_s + loss_fm_f + loss_mel
 
-            loss_gen_all.backward()
-            optim_g.step()
+    #         loss_gen_all.backward()
+    #         optim_g.step()
 
-            if rank == 0:
-                # STDOUT logging
-                if steps % a.stdout_interval == 0:
-                    with torch.no_grad():
-                        mel_error = F.l1_loss(y_mel, y_g_hat_mel).item()
+    #         if rank == 0:
+    #             # STDOUT logging
+    #             if steps % a.stdout_interval == 0:
+    #                 with torch.no_grad():
+    #                     mel_error = F.l1_loss(y_mel, y_g_hat_mel).item()
 
-                    print(
-                        "Steps : {:d}, Gen Loss Total : {:4.3f}, Mel-Spec. Error : {:4.3f}, s/b : {:4.3f}".format(
-                            steps, loss_gen_all, mel_error, time.time() - start_b
-                        )
-                    )
+    #                 print(
+    #                     "Steps : {:d}, Gen Loss Total : {:4.3f}, Mel-Spec. Error : {:4.3f}, s/b : {:4.3f}".format(
+    #                         steps, loss_gen_all, mel_error, time.time() - start_b
+    #                     )
+    #                 )
 
-                # checkpointing
-                if steps % a.checkpoint_interval == 0 and steps != 0:
-                    checkpoint_path = "{}/g_{:08d}".format(a.checkpoint_path, steps)
-                    save_checkpoint(
-                        checkpoint_path,
-                        {
-                            "generator": (
-                                generator.module if h.num_gpus > 1 else generator
-                            ).state_dict()
-                        },
-                    )
-                    checkpoint_path = "{}/do_{:08d}".format(a.checkpoint_path, steps)
-                    save_checkpoint(
-                        checkpoint_path,
-                        {
-                            "mpd": (mpd.module if h.num_gpus > 1 else mpd).state_dict(),
-                            "msd": (msd.module if h.num_gpus > 1 else msd).state_dict(),
-                            "optim_g": optim_g.state_dict(),
-                            "optim_d": optim_d.state_dict(),
-                            "steps": steps,
-                            "epoch": epoch,
-                        },
-                    )
+    #             # checkpointing
+    #             if steps % a.checkpoint_interval == 0 and steps != 0:
+    #                 checkpoint_path = "{}/g_{:08d}".format(a.checkpoint_path, steps)
+    #                 save_checkpoint(
+    #                     checkpoint_path,
+    #                     {
+    #                         "generator": (
+    #                             generator.module if h.num_gpus > 1 else generator
+    #                         ).state_dict()
+    #                     },
+    #                 )
+    #                 checkpoint_path = "{}/do_{:08d}".format(a.checkpoint_path, steps)
+    #                 save_checkpoint(
+    #                     checkpoint_path,
+    #                     {
+    #                         "mpd": (mpd.module if h.num_gpus > 1 else mpd).state_dict(),
+    #                         "msd": (msd.module if h.num_gpus > 1 else msd).state_dict(),
+    #                         "optim_g": optim_g.state_dict(),
+    #                         "optim_d": optim_d.state_dict(),
+    #                         "steps": steps,
+    #                         "epoch": epoch,
+    #                     },
+    #                 )
 
-                # Tensorboard summary logging
-                if steps % a.summary_interval == 0:
-                    sw.add_scalar("training/gen_loss_total", loss_gen_all, steps)
-                    sw.add_scalar("training/mel_spec_error", mel_error, steps)
+    #             # Tensorboard summary logging
+    #             if steps % a.summary_interval == 0:
+    #                 sw.add_scalar("training/gen_loss_total", loss_gen_all, steps)
+    #                 sw.add_scalar("training/mel_spec_error", mel_error, steps)
 
-                # Validation
-                if steps % a.validation_interval == 0:  # and steps != 0:
-                    generator.eval()
-                    torch.cuda.empty_cache()
-                    val_err_tot = 0
-                    with torch.no_grad():
-                        for j, batch in enumerate(validation_loader):
-                            x, y, _, y_mel = batch
-                            y_g_hat = generator(x.to(device))
-                            y_mel = torch.autograd.Variable(
-                                y_mel.to(device, non_blocking=True)
-                            )
-                            y_g_hat_mel = mel_spectrogram(
-                                y_g_hat.squeeze(1),
-                                h.n_fft,
-                                h.num_mels,
-                                h.sampling_rate,
-                                h.hop_size,
-                                h.win_size,
-                                h.fmin,
-                                h.fmax_for_loss,
-                            )
-                            diff_shape = y_g_hat_mel.shape[-1] - y_mel.shape[-1]
-                            y_mel = torch.nn.functional.pad(
-                                y_mel, (0, diff_shape), mode="constant"
-                            )
+    #             # Validation
+    #             if steps % a.validation_interval == 0:  # and steps != 0:
+    #                 generator.eval()
+    #                 torch.cuda.empty_cache()
+    #                 val_err_tot = 0
+    #                 with torch.no_grad():
+    #                     for j, batch in enumerate(validation_loader):
+    #                         x, y, _, y_mel = batch
+    #                         y_g_hat = generator(x.to(device))
+    #                         y_mel = torch.autograd.Variable(
+    #                             y_mel.to(device, non_blocking=True)
+    #                         )
+    #                         y_g_hat_mel = mel_spectrogram(
+    #                             y_g_hat.squeeze(1),
+    #                             h.n_fft,
+    #                             h.num_mels,
+    #                             h.sampling_rate,
+    #                             h.hop_size,
+    #                             h.win_size,
+    #                             h.fmin,
+    #                             h.fmax_for_loss,
+    #                         )
+    #                         diff_shape = y_g_hat_mel.shape[-1] - y_mel.shape[-1]
+    #                         y_mel = torch.nn.functional.pad(
+    #                             y_mel, (0, diff_shape), mode="constant"
+    #                         )
 
-                            val_err_tot += F.l1_loss(y_mel, y_g_hat_mel).item()
+    #                         val_err_tot += F.l1_loss(y_mel, y_g_hat_mel).item()
 
-                            if j <= 4:
-                                if steps == 0:
-                                    sw.add_audio(
-                                        "gt/y_{}".format(j),
-                                        y[0],
-                                        steps,
-                                        h.sampling_rate,
-                                    )
-                                    sw.add_figure(
-                                        "gt/y_spec_{}".format(j),
-                                        plot_spectrogram(x[0]),
-                                        steps,
-                                    )
+    #                         if j <= 4:
+    #                             if steps == 0:
+    #                                 sw.add_audio(
+    #                                     "gt/y_{}".format(j),
+    #                                     y[0],
+    #                                     steps,
+    #                                     h.sampling_rate,
+    #                                 )
+    #                                 sw.add_figure(
+    #                                     "gt/y_spec_{}".format(j),
+    #                                     plot_spectrogram(x[0]),
+    #                                     steps,
+    #                                 )
 
-                                sw.add_audio(
-                                    "generated/y_hat_{}".format(j),
-                                    y_g_hat[0],
-                                    steps,
-                                    h.sampling_rate,
-                                )
-                                y_hat_spec = mel_spectrogram(
-                                    y_g_hat.squeeze(1),
-                                    h.n_fft,
-                                    h.num_mels,
-                                    h.sampling_rate,
-                                    h.hop_size,
-                                    h.win_size,
-                                    h.fmin,
-                                    h.fmax,
-                                )
-                                sw.add_figure(
-                                    "generated/y_hat_spec_{}".format(j),
-                                    plot_spectrogram(
-                                        y_hat_spec.squeeze(0).cpu().numpy()
-                                    ),
-                                    steps,
-                                )
+    #                             sw.add_audio(
+    #                                 "generated/y_hat_{}".format(j),
+    #                                 y_g_hat[0],
+    #                                 steps,
+    #                                 h.sampling_rate,
+    #                             )
+    #                             y_hat_spec = mel_spectrogram(
+    #                                 y_g_hat.squeeze(1),
+    #                                 h.n_fft,
+    #                                 h.num_mels,
+    #                                 h.sampling_rate,
+    #                                 h.hop_size,
+    #                                 h.win_size,
+    #                                 h.fmin,
+    #                                 h.fmax,
+    #                             )
+    #                             sw.add_figure(
+    #                                 "generated/y_hat_spec_{}".format(j),
+    #                                 plot_spectrogram(
+    #                                     y_hat_spec.squeeze(0).cpu().numpy()
+    #                                 ),
+    #                                 steps,
+    #                             )
 
-                        val_err = val_err_tot / (j + 1)
-                        sw.add_scalar("validation/mel_spec_error", val_err, steps)
+    #                     val_err = val_err_tot / (j + 1)
+    #                     sw.add_scalar("validation/mel_spec_error", val_err, steps)
 
-                    generator.train()
+    #                 generator.train()
 
-            steps += 1
+    #         steps += 1
 
-        scheduler_g.step()
-        scheduler_d.step()
+    #     scheduler_g.step()
+    #     scheduler_d.step()
 
-        if rank == 0:
-            print(
-                "Time taken for epoch {} is {} sec\n".format(
-                    epoch + 1, int(time.time() - start)
-                )
-            )
+    #     if rank == 0:
+    #         print(
+    #             "Time taken for epoch {} is {} sec\n".format(
+    #                 epoch + 1, int(time.time() - start)
+    #             )
+    #         )
 
 
 def main():
